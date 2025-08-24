4b2d0750a6415aad2bee175ec92ce1098078fa2b
Vadym Ustinov <ustinov.va@gmail.com>
2025-08-24 14:48:57 +0200

    Add IAM plicy policy_deny_iam_all

--- policies.tf
+++ policies.tf
@@ -28,7 +28,33 @@ module "policy_deny_console_access" {
   })
 }
 
+module "policy_deny_iam_all" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "6.2.0"
+
+  providers = {
+    aws = aws.acc0
+  }
+
+  name = "deny-iam-all"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Deny",
+      Action   = ["iam:*"],
+      Resource = "*"
+    }]
+  })
+}
+
+
+# Outputs
 output "deny_console_access_policy_arn" {
   description = "ARN of deny-console-access policy"
   value       = module.policy_deny_console_access.arn
-}   
+}
+
+output "policy_deny_iam_all_arn" {
+  description = "ARN of deny-iam-all policy"
+  value       = module.policy_deny_iam_all.arn
+}
