17b2e3288532198516a184cecd9f0edf50aee9c2
Vadym Ustinov <ustinov.va@gmail.com>
2025-08-24 15:26:24 +0200

    Add policy for assume role

--- policies.tf
+++ policies.tf
@@ -28,6 +28,7 @@ module "policy_deny_console_access" {
   })
 }
 
+# Allow S3 access
 module "policy_deny_iam_all" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version = "6.2.0"
@@ -47,6 +48,52 @@ module "policy_deny_iam_all" {
   })
 }
 
+#Allow S3 access 
+module "policy_s3" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "6.2.0"
+
+  providers = {
+    aws = aws.acc1
+  }
+
+  name = "s3-access"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:ListBucket"],
+        Resource = ["arn:aws:s3:::${local.s3_bucket_name}"]
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject","s3:PutObject","s3:DeleteObject"],
+        Resource = ["arn:aws:s3:::${local.s3_bucket_name}/*"]
+      }
+    ]
+  })
+}
+
+# Allow assumeRole to roleC (acc1)
+module "policy_allow_assume_roleC" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "6.2.0"
+
+  providers = {
+    aws = aws.acc0
+  }
+
+  name = "allow-assume-roleC"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["sts:AssumeRole"],
+      Resource = local.roleC_arn
+    }]
+  })
+}
 
 # Outputs
 output "deny_console_access_policy_arn" {
@@ -57,4 +104,14 @@ output "deny_console_access_policy_arn" {
 output "policy_deny_iam_all_arn" {
   description = "ARN of deny-iam-all policy"
   value       = module.policy_deny_iam_all.arn
+}
+
+output "policy_s3_arn" {
+  description = "ARN of roleC S3 policy (acc1)"
+  value       = module.policy_s3.arn
+}
+
+output "policy_allow_assume_roleC_arn" {
+  description = "ARN of allow-assume-roleC policy"
+  value       = module.policy_allow_assume_roleC.arn
 }
